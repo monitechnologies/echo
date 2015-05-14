99531f1111fbf08048c6aa9a74cdd79e481d7ed2
Michael Donat <michael.donat@me.com>
2015-05-14 08:30:46 +0100

    returning just the session id

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post('/', function(request, response) {
 
 app.get('/', function(request, response) {
 
-  response.json(store[store.length - 1])
+  response.json(store[store.length - 1]['session_id'])
 
 })
 
